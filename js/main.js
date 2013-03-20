5d34feb4394762181b862be47e1002ecb88829da
1000ch <orz1000ch@gmail.com>
2013-03-20 14:40:31 +0900

    brush up

--- js/main.js
+++ js/main.js
@@ -1,23 +1,51 @@
 /**
- * OneClickClearHistory
+ * OneClickClear
  * Easy to delete history.
  *
  * Copyright 1000ch<http://1000ch.net/>
  */
-OneClickClearHistory = {
-	init: function() {
-		chrome.browserAction.onClicked.addListener(function(tab) {
-			if(confirm("Are you sure to erase all history?")) {
-				chrome.history.deleteAll(function() {});
-			}
+
+/**
+ * general ready function
+ * @param {Object} caller
+ * @param {Function} callback
+ */
+var ready = function(caller, callback) {
+	if(document.readyState === "complete" || document.readyState === "interactive") {
+		callback.call(caller);
+	} else {
+		document.addEventListener("DOMContentLoaded", function() {
+			callback.call(caller);
 		});
 	}
 };
 
-if(document.readyState === "complete" || document.readyState === "interactive") {
-	OneClickClearHistory.init();
-} else {
-	document.addEventListener("DOMContentLoaded", function() {
-		OneClickClearHistory.init();
-	});
-}
+OneClickClear = {
+	/**
+	 * init function
+	 */
+	init: function() {
+		//update badge once.
+		this.updateBadgeText();
+
+		//start listening
+		chrome.browserAction.onClicked.addListener(function(tab) {
+			if(window.confirm("Are you sure you want to erase all history?\n(This action cannot be canceled)")) {
+				chrome.history.deleteAll(OneClickClear.updateBadgeText);
+			}
+		});
+	},
+	/**
+	 * set history count as badge text
+	 */
+	updateBadgeText: function() {
+		chrome.history.search({text: ""}, function(items) {
+			chrome.browserAction.setBadgeText({
+				text: String(items.length)
+			});
+		});
+	}
+};
+
+//when document is ready, call init
+ready(OneClickClear, OneClickClear.init);
